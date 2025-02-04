648acddb6cc0131b7a55966a10dec29842d2fa88
Arif Khan <arifkhan50636@gmail.com>
2025-02-04 17:02:49 +0530

    soda

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ person = input('are you coming for walk?: ')
 if person=='yes':
     print('I will give something')
     print('How are you')
+    print('dsfklsdfjgks')
+    print('jslfsdkjf')
 else:
     person2 = input('Why are you not coming')
     if person2=='yes':
